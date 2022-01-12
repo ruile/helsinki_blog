937dfc688fdd60986a342f63c9c3fb70900df7bc
ruile <ruilegan@outlook.com>
2022-01-12 22:24:46 +0800

    added first token authentication using jwt

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -12,7 +22,10 @@ blogsRouter.get('/', async (request, response) => {
     //   })
 
     // async - await syntax
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', {
+        username: 1,
+        name: 1
+    })
     response.json(blogs)
 })
 
@@ -30,7 +43,21 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
 
     const body = request.body
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+
+    // contains username and id fields
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    
+    // blog
+    //   .save()
+    //   .then(result => {
+    //     response.status(201).json(result)
+    //   })
 
     const blog = new Blog({
         title: body.title,
@@ -40,15 +67,11 @@ blogsRouter.post('/', async (request, response) => {
         user: user._id
     })
   
-    // blog
-    //   .save()
-    //   .then(result => {
-    //     response.status(201).json(result)
-    //   })
 
     const savedBlog = await blog.save()
 
     user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
 
     response.status(201).json(savedBlog)
 })
